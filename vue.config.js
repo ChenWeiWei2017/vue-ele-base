f4e39320d30fab263e0e97044f1457be36b82897
ChenWeiWei2017 <32676571+ChenWeiWei2017@users.noreply.github.com>
2020-09-16 15:54:39 +0800

    初始配置

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+function resolve(dir) {
+  // __dirname 当前模块目录绝对路径
+  return path.join(__dirname, dir)
+}
+
+const name = 'vue-manage-system'
+
+module.exports = {
+  devServer: {
+    open: true,
+    port: 8080
+  },
+
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        // 配置@指向src目录
+        '@': resolve('src')
+      }
+    }
+  },
+
+  css: {
+    loaderOptions: {
+      sass: {
+        // 引入全局的样式变量 旧键名为data，新键名为prependData
+        prependData: `@import "@/assets/scss/_variable.scss";`
+      }
+    }
+  }
+}
